d053056c0900e16631b822b7250634202d715b67
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2025-06-05 15:22:21 -0500

    y

--- app/browse/page.jsx
+++ app/browse/page.jsx
@@ -175,10 +175,3 @@ export default function BrowsePage() {
             </div>
     )
 }
-
-    // return (
-    //     <div className="flex flex-col items-center justify-center h-screen bg-neutral-900 text-white">
-    //         <h1 className="text-4xl font-bold mb-4">Browse Movies</h1>
-    //         <p className="text-lg">Explore a wide range of movies and shows.</p>
-    //     </div>
-    // );w-
