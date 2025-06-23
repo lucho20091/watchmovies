a83e142d61714045d319adc3b45027230368b988
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2025-06-22 20:29:27 -0500

    ye

--- app/api/movie/[id]/route.js
+++ app/api/movie/[id]/route.js
@@ -1,14 +1,19 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
-    const { id } = await params;
-    try{
-        const response = await fetch(`https://vidsrc.xyz/embed/movie?tmdb=${id}&ds_lang=es&autoplay=1`);
-        console.log(response.url);
-        // const data = await response.json();
-        return NextResponse.json(response.url);
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
-    }
+  const { id } = await params;
+  try {
+    const response = await fetch(
+      `https://vidsrc.me/embed/movie?tmdb=${id}&ds_lang=es&autoplay=1`
+    );
+    console.log(response.url);
+    // const data = await response.json();
+    return NextResponse.json(response.url);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
 }
