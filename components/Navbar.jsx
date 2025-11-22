2abf326e5fb4ec54f5081d145fa0f6caf8c5db66
[dyad] <darkness_20_09@hotmail.com>
2025-11-22 16:03:28 -0500

    Updated Navbar background color - wrote 1 file(s)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -74,7 +74,7 @@ export default function Navbar() {
   }, [isSearchInputVisible]);
 
   return (
-    <nav className="sticky top-0 left-0 right-0 z-50 bg-rich-mahogany-800 py-4 px-6 shadow-lg border-b border-gray-800">
+    <nav className="sticky top-0 left-0 right-0 z-50 bg-rich-mahogany-900 py-4 px-6 shadow-lg border-b border-gray-800">
       <div className="container mx-auto flex justify-between items-center">
         <Link
           href="/"
